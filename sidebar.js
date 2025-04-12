995ebea8d534b0a475befeb405bd2efc43fb150e
congdeptrycoder <congchiss.it@gmail.com>
2025-04-13 00:08:02 +0700

    update index, responsive index

--- sidebar.js
+++ sidebar.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleBtn = document.querySelector('.side-bar-toggle svg');
     const sideBarMenu = document.querySelector('.side-bar-menu');
-    // Xử lý click toggle button
     toggleBtn.addEventListener('click', function (e) {
         e.stopPropagation();
         this.classList.toggle('active');
         sideBarMenu.classList.toggle('active');
     });
 
-    // Đóng menu khi click ra ngoài
     document.addEventListener('click', function (e) {
         if (!sideBarMenu.contains(e.target) && e.target !== toggleBtn) {
             toggleBtn.classList.remove('active');
@@ -16,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Xử lý dropdown menu trên mobile
+
     const dropdownParents = document.querySelectorAll('.side-bar-menu > ul > li:has(.dropdown-menu)');
     dropdownParents.forEach(parent => {
         const link = parent.querySelector('a');
